49e8e6719080ccb66cc42787e70f4cb61dec361e
H1k4r1 <lhkkennedy@gmail.com>
2023-02-11 02:29:20 +0000

    remove unecessary files

--- beathub/Python/parser.py
+++ beathub/Python/parser.py
@@ -4,5 +4,7 @@ columns = ['index', 'time-stamp', 'in/out', 'verbose', 'channel', 'value', 'hex'
 targetColumns = ['midi-item-name', 'midi-item-code', 'midi-item-values']
 
 df = pd.read_csv('beathub\Python\midi\startstop_Midi.csv', header=None, names=columns)
+df2 = pd.read_csv('beathub\Python\midiDict.csv')
 
 print(df)
+print(df2)
